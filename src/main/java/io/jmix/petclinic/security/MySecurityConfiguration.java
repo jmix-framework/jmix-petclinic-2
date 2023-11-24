3b28f9eb63341ee99f388c8074a6ab1adeb9060e
Konstantin Krivopustov <krivopustov@haulmont.com>
2023-11-24 17:24:36 +0400

    Upgrade to Jmix 2.1.1

--- src/main/java/io/jmix/petclinic/security/MySecurityConfiguration.java
+++ src/main/java/io/jmix/petclinic/security/MySecurityConfiguration.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 public class MySecurityConfiguration {
@@ -14,7 +15,8 @@ public class MySecurityConfiguration {
     @Order(JmixSecurityFilterChainOrder.FLOWUI - 10)
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.securityMatcher("/images/**")
-                .authorizeHttpRequests((authorize) -> authorize.requestMatchers("/images/**").permitAll());
+                .authorizeHttpRequests((authorize) ->
+                        authorize.requestMatchers(AntPathRequestMatcher.antMatcher("/images/**")).permitAll());
         return http.build();
     }
 }
