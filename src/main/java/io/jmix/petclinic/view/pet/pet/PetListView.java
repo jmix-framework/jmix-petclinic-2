9bcfce19e46302b67a9cfcc5c6a7b87fe7faed5d
Konstantin Krivopustov <krivopustov@haulmont.com>
2023-11-27 11:31:49 +0400

    Fix filter cleaning

--- src/main/java/io/jmix/petclinic/view/pet/pet/PetListView.java
+++ src/main/java/io/jmix/petclinic/view/pet/pet/PetListView.java
@@ -1,5 +1,6 @@
 package io.jmix.petclinic.view.pet.pet;
 
+import io.jmix.flowui.component.propertyfilter.PropertyFilter;
 import io.jmix.flowui.kit.action.ActionPerformedEvent;
 import io.jmix.petclinic.entity.pet.Pet;
 
@@ -14,9 +15,19 @@ import io.jmix.flowui.view.*;
 @LookupComponent("petsDataGrid")
 @DialogMode(width = "50em")
 public class PetListView extends StandardListView<Pet> {
+
+    @ViewComponent
+    private PropertyFilter identificationNumberFilter;
+    @ViewComponent
+    private PropertyFilter typeFilter;
+    @ViewComponent
+    private PropertyFilter ownerFilter;
+
     @Subscribe("clearFilterAction")
     public void onClearFilterAction(final ActionPerformedEvent event) {
-
+        identificationNumberFilter.clear();
+        typeFilter.clear();
+        ownerFilter.clear();
     }
 
 }
