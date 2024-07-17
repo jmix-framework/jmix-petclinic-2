c7eae436ee7fb7f5d221241a6dc549aa0c7d475d
Dmitry Kremnev <kremnev.dima@mail.ru>
2024-07-17 18:07:15 +0400

    Styling and counter of visits in main menu jmix-framework/jmix-petclinic-2#2

--- src/main/java/io/jmix/petclinic/view/main/MainView.java
+++ src/main/java/io/jmix/petclinic/view/main/MainView.java
@@ -1,14 +1,18 @@
 package io.jmix.petclinic.view.main;
 
 import com.vaadin.flow.component.html.Image;
+import com.vaadin.flow.component.html.Span;
 import com.vaadin.flow.component.orderedlayout.FlexComponent;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
+import io.jmix.core.DataManager;
+import io.jmix.core.security.CurrentAuthentication;
 import io.jmix.flowui.UiComponents;
 import io.jmix.flowui.app.main.StandardMainView;
-import io.jmix.flowui.view.Subscribe;
-import io.jmix.flowui.view.ViewController;
-import io.jmix.flowui.view.ViewDescriptor;
+import io.jmix.flowui.component.main.JmixListMenu;
+import io.jmix.flowui.facet.Timer;
+import io.jmix.flowui.kit.component.main.ListMenu;
+import io.jmix.flowui.view.*;
 import io.jmix.petclinic.online.OnlineDemoDataCreator;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -20,8 +24,20 @@ public class MainView extends StandardMainView {
     @Autowired
     private UiComponents uiComponents;
 
+    @Autowired
+    private MessageBundle messageBundle;
+
+    @Autowired
+    private DataManager dataManager;
+
+    @Autowired
+    private CurrentAuthentication currentAuthentication;
+
     @Autowired(required = false)
     private OnlineDemoDataCreator onlineDemoDataCreator;
+
+    @ViewComponent
+    private JmixListMenu menu;
 
     @Subscribe
     public void onInit(final InitEvent event) {
@@ -36,6 +52,8 @@ public class MainView extends StandardMainView {
         verticalLayout.setHeightFull();
         verticalLayout.setWidthFull();
         getContent().showRouterLayoutContent(verticalLayout);
+
+        initMyVisitBadge();
     }
 
     @Subscribe
@@ -43,5 +61,34 @@ public class MainView extends StandardMainView {
         if (onlineDemoDataCreator != null) {
             onlineDemoDataCreator.createDemoData();
         }
+    }
+
+    @Subscribe("refreshMyVisitsBadge")
+    public void onRefreshMyVisitsBadgeTimerAction(final Timer.TimerActionEvent event) {
+        ListMenu.MenuItem menuItem = menu.getMenuItem("petclinic_MyVisits");
+
+        if (menuItem != null && menuItem.getSuffixComponent() instanceof Span badge) {
+            badge.setText(messageBundle.formatMessage("myVisitMenuItemBadge.text", calculateAmountOfVisits()));
+        }
+    }
+
+    private void initMyVisitBadge() {
+        Span badge = uiComponents.create(Span.class);
+        badge.setText(messageBundle.formatMessage("myVisitMenuItemBadge.text", calculateAmountOfVisits()));
+        badge.getElement().getThemeList().add("badge warning");
+
+        ListMenu.MenuItem menuItem = menu.getMenuItem("petclinic_MyVisits");
+        if (menuItem != null) {
+            menuItem.setSuffixComponent(badge);
+        }
+    }
+
+    private long calculateAmountOfVisits() {
+        return dataManager.loadValue("select count(e) from petclinic_Visit e " +
+                                "where e.assignedNurse = :currentUser " +
+                                "and e.treatmentStatus <> @enum(io.jmix.petclinic.entity.visit.VisitTreatmentStatus.DONE)",
+                        Long.class)
+                .parameter("currentUser", currentAuthentication.getUser())
+                .one();
     }
 }
