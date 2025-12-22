20cc9181ed4435f1efa06a4cdfe9139470b67a64
Dmitry Kremnev <kremnev.dima@mail.ru>
2025-12-22 11:15:45 +0400

    Health check request jmix-framework/jmix-petclinic-2#7

---
+++ src/main/java/io/jmix/petclinic/security/PetclinicSecurityConfiguration.java
@@ -0,0 +1,23 @@
+package io.jmix.petclinic.security;
+
+import io.jmix.core.JmixSecurityFilterChainOrder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class PetclinicSecurityConfiguration {
+
+    @Bean
+    @Order(JmixSecurityFilterChainOrder.CUSTOM)
+    SecurityFilterChain publicFilterChain(HttpSecurity http) throws Exception {
+        http.securityMatcher("/healthcheck")
+                .authorizeHttpRequests(authorize ->
+                        authorize.anyRequest().permitAll()
+                );
+
+        return http.build();
+    }
+}
